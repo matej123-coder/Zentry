6931b103c568afd3819e35138cf45c63a8c50dc0
matej123-coder <mmatej.minoski@gmail.com>
2026-04-21 16:02:49 +0200

    feat: setup the fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ export default {
   content: [ "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        zentry: ['zentry','sans-serif'],
+        general: ['general','sans-serif'],
+        'circular-web':['circular-web','sans-serif'],
+        'robert-medium':['robert-regular','sans-serif'],
+        'robert-regular':['robert-regular','sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
