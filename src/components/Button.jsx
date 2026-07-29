083a574527a960228395e8fcbaa5f3d20253a293
matej123-coder <mmatej.minoski@gmail.com>
2026-07-29 13:51:27 +0200

    fix: add hover to buttons and apply the footer

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,9 @@ const Button = ({title,id,leftIcon,containerClass,rightIcon}) =>{
     return(
         <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}` }>
             {leftIcon}
-            <span className="relative incline-flex overflow-hidden font-general text-xs uppercase">
-                <div >{title}</div>
+            <span className="relative inline-flex overflow-hidden font-general text-xs uppercase">
+                <div className="translate-y-0 skew-y-0 transition duration-500 group-hover:translate-y-[-160%] group-hover:skew-y-12">{title}</div>
+                <div className="absolute translate-y-[164%] skew-y-12 transition duration-500 group-hover:translate-y-0 group-hover:skew-y-0">{title}</div>
             </span>
             {rightIcon}
         </button>
